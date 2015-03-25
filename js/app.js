e5bdeccb814000a271ff9d4ad9355dbe71bfdc40
Greazy6 <grclkrc@hotmail.com>
2015-03-25 18:46:51 -0400

    comments….comments

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
+
+//this above function is to randomize the speed of the ladybugs
 var randomSpeed = function(){
     var runSpeed = (Math.floor(Math.random()* 5) + 1) * 75;
     return runSpeed;
 }
 
+//this is to randomize the row of the gem
 var row = [60, 145, 230];
 var randomRow = function(){
     getRandomRow = row[Math.floor(Math.random()* row.length)];
     return getRandomRow;
 }
-
+//this is to randomize the column of the gem
+//these two took a while to figure out
 var column = [0,101, 202, 303, 404, 505, 606];
 var randomColumn = function(){
     getRandomColumn = column[Math.floor(Math.random()* column.length)];
     return getRandomColumn;
 }
-
+//this is a gem....this is its object
 var Gem = function(x,y){
     this.sprite = "images/Gem-Blue.png";
     this.x = x;
     this.y = y;
 }
-
+//enemy object
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
@@ -50,13 +54,13 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-
+//this is my player
 var Player = function(x,y){
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
 }
-
+//this contains the player reset...you hit the water you reset
 Player.prototype.update = function(dt){
     if(this.y < 50){
         player.reset();
@@ -83,7 +87,8 @@ Player.prototype.update = function(dt){
 }
   
 }
-
+//the above collision took forever to figure out
+//below is my movment
 Player.prototype.handleInput = function(direction){
     if(direction === 'left' && this.x > 25){
         this.x -=100;
@@ -100,7 +105,7 @@ Player.prototype.handleInput = function(direction){
 }
 
 
-
+//renders the gem
 
 Gem.prototype.render= function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -121,7 +126,7 @@ Player.prototype.reset = function(){
 }
 
 
-
+//6 emenys, yeah thats right 6 enemeys on a small board it will make the game hard
 
 var enemy1 = new Enemy(0, 60, 100);
 var enemy2 = new Enemy(0, 145, 200);
@@ -131,7 +136,7 @@ var enemy5 = new Enemy(0, 60, 120);
 var enemy6 = new Enemy(0, 230, 250);
 
 var allEnemies = [enemy1,enemy2,enemy3,enemy4, enemy5, enemy6];
-
+// new player
 var player = new Player(200, 400);
 
 var gem = new Gem(randomColumn(), randomRow());
